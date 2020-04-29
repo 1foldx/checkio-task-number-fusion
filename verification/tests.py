7c68bb849aa18195f5a36485289337a6b2c08d2b
1foldx <55111988+1foldx@users.noreply.github.com>
2020-04-29 14:37:00 +0000

    Update tests.py
    First tests

--- verification/tests.py
+++ verification/tests.py
@@ -7,30 +7,39 @@ Each test is a dict with
     "explanation" -- not necessarily a key, it's used for an additional info in animation.
 """
 
+"""
+TESTS is a dict with all of your tests.
+Keys for this will be the categories' names.
+Each test is a dict with
+    "input" -- input data for a user function
+    "answer" -- your right answer
+    "explanation" -- not necessarily a key, it's used for an additional info in animation.
+"""
+
 
 TESTS = {
     "Basics": [
         {
-            "input": [2],
-            "answer": True,
+            "input": [0, 9],
+            "answer": 9
         },
         {
-            "input": [5],
-            "answer": False,
+            "input": [9, 0],
+            "answer": 90
         },
         {
-            "input": [0],
-            "answer": True,
+            "input": [90000, 11111],
+            "answer": 9101010101
         },
     ],
     "Extra": [
         {
-            "input": [6],
-            "answer": True,
+            "input": [0, 0],
+            "answer": 0,
         },
-        {
-            "input": [17],
-            "answer": False,
+                {
+            "input": [10, 10],
+            "answer": 1100,
         },
     ]
 }
